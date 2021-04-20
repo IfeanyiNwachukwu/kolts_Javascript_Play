fe4e63f7e819d873017708a9eb69a57dbad6f04f
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-04-20 01:59:03 +0100

    committing initial changes

---
+++ js/string.js
@@ -0,0 +1,73 @@
+// let msg = "you are so grounded mr"
+// console.log(msg.toUpperCase());
+// console.log(msg.toLowerCase());
+
+// let color = "       purple        "
+
+// console.log(color);
+// console.log(color.trim());
+
+// console.log(color.trim().toUpperCase());
+
+// INDEX OF Returns the position of a char in a string
+// let tvShow = 'catdog';
+
+// console.log(tvShow.indexOf('cat'));
+// console.log(tvShow.indexOf('dog'));
+// console.log(tvShow.indexOf('z'));
+
+// SLICE --Takes a slice of an existing strig and returns it to you
+// var slice1 = "Baseball".slice(4);
+// console.log(slice1);
+// var slice2 = "superhero".slice(0,5);
+// console.log(slice2);
+// var slice3 = "superhero".slice(0,5);
+// console.log(slice3);
+
+// // REPLACE
+
+// let annoyingLaugh = 'teehee so funny! teehee !';
+// let betterLagh = annoyingLaugh.replace('teehee','hahaha');
+
+// console.log(betterLagh);
+
+// let yell = 'GO AWAY!!';
+// let index1 = yell.indexOf('!');
+// let index2 = yell.lastIndexOf('!');
+// console.log(index1);
+// console.log(index2);
+
+
+// STRING TEMPLATE LITERALS
+
+console.log(`I counted ${3 + 4} sheeps`); // Note: Template literals are enclosed by back ticks
+
+let template = `I counted ${3 + 4} sheeps`;
+console.log(template);
+
+let userName = 'Ziggy31';
+console.log(`Welcome back ${userName}`);
+console.log(`GAME OVER ${userName.toUpperCase()}`);
+
+let animal = "pig";
+let sound = "oink";
+
+console.log(`${animal} says ${sound}`);
+
+let item = 'cucumber';
+let price = 1.99;
+let quantity = 4;
+
+console.log(`You bought ${quantity} ${item}, total price: $${price * quantity}`);
+
+const minAge = 21;
+let yourAge = 19;
+
+console.log(`You must be ${minAge} or older to enter. Come back in ${minAge - yourAge} years`);
+
+
+
+
+
+
+
